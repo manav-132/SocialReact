c89d99a8ffb899b6dd53247c22b1f5e73e195ee0
Manav joshi <manavjoshi1107@gmail.com>
2024-08-04 23:51:28 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import replace from '@rollup/plugin-replace';
+import path from 'path';
 
 export default defineConfig({
   plugins: [
@@ -9,6 +10,15 @@ export default defineConfig({
       'process.env': JSON.stringify(process.env),
     }),
   ],
+  resolve: {
+    alias: {
+      '@emotion/react': path.resolve(__dirname, 'node_modules/@emotion/react'),
+      '@emotion/styled': path.resolve(__dirname, 'node_modules/@emotion/styled'),
+      '@mui/material': path.resolve(__dirname, 'node_modules/@mui/material'),
+      '@mui/system': path.resolve(__dirname, 'node_modules/@mui/system'),
+      '@mui/styled-engine': path.resolve(__dirname, 'node_modules/@mui/styled-engine'),
+    },
+  },
   build: {
     rollupOptions: {
       external: [
