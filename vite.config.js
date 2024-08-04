c7dfd191ae5cf4152a67716006ef0d9e41ca8572
Manav joshi <manavjoshi1107@gmail.com>
2024-08-04 23:38:40 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import replace from '@rollup/plugin-replace';
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     replace({
       'process.env': JSON.stringify(process.env),
     }),
   ],
-})
-
-
-
-
-
-
+  build: {
+    rollupOptions: {
+      external: [
+        '@material-ui/icons',
+        '@emotion/react',
+        '@emotion/styled',
+        '@mui/material',
+        '@mui/system',
+        '@mui/styled-engine'
+      ],
+    },
+  },
+});
