6cc11a152076bc60f65ef5645afc5a85ec9ef112
manav-132 <manavjoshi1107@gmail.com>
2024-08-05 13:16:59 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import replace from '@rollup/plugin-replace';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    replace({
-      'process.env': JSON.stringify(process.env),
-    }),
-  ],
+  plugins: [react()],
+  optimizeDeps: {
+    include: ["@material-ui/icons"]
+  },
   resolve: {
     alias: {
-      '@emotion/react': path.resolve(__dirname, 'node_modules/@emotion/react'),
-      '@emotion/styled': path.resolve(__dirname, 'node_modules/@emotion/styled'),
-      '@mui/material': path.resolve(__dirname, 'node_modules/@mui/material'),
-      '@mui/system': path.resolve(__dirname, 'node_modules/@mui/system'),
-      '@mui/styled-engine': path.resolve(__dirname, 'node_modules/@mui/styled-engine'),
-    },
-  },
-  build: {
-    rollupOptions: {
-      external: [
-        '@material-ui/icons',
-        '@emotion/react',
-        '@emotion/styled',
-        '@mui/material',
-        '@mui/system',
-        '@mui/styled-engine'
-      ],
-    },
-  },
-});
+      '@material-ui/icons': '@material-ui/icons/esm'
+    }
+  }
+})
