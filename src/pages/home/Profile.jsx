f07a3d782d536d086460ae93070d4c3b8ad16660
manav-132 <manavjoshi1107@gmail.com>
2024-08-05 13:50:53 +0530

    Update Profile.jsx

--- src/pages/home/Profile.jsx
+++ src/pages/home/Profile.jsx
@@ -4,19 +4,18 @@ import Leftbar from '../../components/leftbar/Leftbar';
 import Feed from '../../components/feed/Feed';
 import Rightbar from '../../components/rightbar/Rightbar';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 export default function Profile() {
   const [user, setUser] = useState({});
-  const Id=user._id
-  console.log(Id)
   const [newProfilePicture, setNewProfilePicture] = useState(null);
   const [newCoverPicture, setNewCoverPicture] = useState(null);
   const [showProfileModal, setShowProfileModal] = useState(false);
   const [showCoverModal, setShowCoverModal] = useState(false);
-  const username = useParams().username;
+  const { username } = useParams();
+  const navigate = useNavigate();
   const PF = "https://social-7tc4.onrender.com/images/";
-  const AF="https://social-7tc4.onrender.com/images/"
+  const AF = "https://social-7tc4.onrender.com/images/";
 
   const handleProfilePictureChange = (e) => {
     setNewProfilePicture(e.target.files[0]);
@@ -29,57 +28,59 @@ export default function Profile() {
   };
 
   const handleSubmitProfile = async (e) => {
-    e.preventDefault()
-    const newpost={
-      userId:user._id,
-      
-    }
-    if(newProfilePicture){
-      const data=new FormData()
-      const filename=newProfilePicture.name
-      console.log(newProfilePicture.name)
-      data.append("file",newProfilePicture)
-      data.append("name",filename)
-      newpost.profilePicture=filename;
+    e.preventDefault();
+    const newpost = {
+      userId: user._id,
+    };
+
+    if (newProfilePicture) {
+      const data = new FormData();
+      const filename = newProfilePicture.name;
+      data.append("file", newProfilePicture);
+      data.append("name", filename);
+      newpost.profilePicture = filename;
+
       try {
-        await axios.post("https://social-7tc4.onrender.com/api/uploadprofile",data)
+        await axios.post("https://social-7tc4.onrender.com/api/uploadprofile", data);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
     }
-    try{
-      await axios.put(`https://social-7tc4.onrender.com/api/users/${Id}`,newpost)
-      window.location.reload()
-    }catch(err){
 
+    try {
+      await axios.put(`https://social-7tc4.onrender.com/api/users/${user._id}`, newpost);
+      setUser(prevUser => ({ ...prevUser, profilePicture: newpost.profilePicture }));
+    } catch (err) {
+      console.log(err);
     }
     setShowProfileModal(false);
   };
 
   const handleSubmitCover = async (e) => {
-    e.preventDefault()
-    const newpost={
-      userId:user._id,
-      
-    }
-    if(newCoverPicture){
-      const data=new FormData()
-      const filename=newCoverPicture.name
-      console.log(newCoverPicture.name)
-      data.append("file",newCoverPicture)
-      data.append("name",filename)
-      newpost.coverPicture=filename;
+    e.preventDefault();
+    const newpost = {
+      userId: user._id,
+    };
+
+    if (newCoverPicture) {
+      const data = new FormData();
+      const filename = newCoverPicture.name;
+      data.append("file", newCoverPicture);
+      data.append("name", filename);
+      newpost.coverPicture = filename;
+
       try {
-        await axios.post("https://social-7tc4.onrender.com/api/uploadcover",data)
+        await axios.post("https://social-7tc4.onrender.com/api/uploadcover", data);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
     }
-    try{
-      await axios.put(`https://social-7tc4.onrender.com/api/users/${Id}`,newpost)
-      window.location.reload()
-    }catch(err){
 
+    try {
+      await axios.put(`https://social-7tc4.onrender.com/api/users/${user._id}`, newpost);
+      setUser(prevUser => ({ ...prevUser, coverPicture: newpost.coverPicture }));
+    } catch (err) {
+      console.log(err);
     }
     setShowCoverModal(false);
   };
@@ -99,7 +100,7 @@ export default function Profile() {
           <div id="profiletop">
             <div className='h-80 relative'>
               <img
-                src={user.coverPicture ? PF + user.coverPicture : {newCoverPicture}}
+                src={user.coverPicture ? PF + user.coverPicture : "default_cover_picture_url"}
                 className='w-full h-64 object-cover cursor-pointer'
                 onClick={() => document.getElementById('coverPictureInput').click()}
               />
@@ -116,7 +117,7 @@ export default function Profile() {
                 </div>
               )}
               <img
-                src={user.profilePicture ? AF + user.profilePicture : ""}
+                src={user.profilePicture ? AF + user.profilePicture : "default_profile_picture_url"}
                 className='w-40 h-40 rounded-full object-cover absolute left-0 right-0 m-auto top-[150px] border-[5px] border-black cursor-pointer'
                 onClick={() => document.getElementById('profilePictureInput').click()}
               />
